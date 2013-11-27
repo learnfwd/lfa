1a7071ca4500391d06eea4f27e3ab4663bd9ebae
Theodor Vararu <theo@tvararu.ro>
2013-11-27 19:34:33 +0200

    Initial actual build and deploy

---
+++ js/main.js
@@ -0,0 +1,1 @@
+(function(){require.config({paths:{jquery:"//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min"}}),require(["jquery"],function(a){return console.log("jquery loaded")})}).call(this)
--- readme.md
+++
@@ -1,1 +0,0 @@
-This is the github-pages branch for [LearnForward-Author](http://github.com/learnfwd/lfa).
