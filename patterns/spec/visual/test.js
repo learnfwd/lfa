df0db468afa6edcd2ffd145278f4fc5c5c7572f8
Theodor Vararu <theo@tvararu.ro>
2013-12-18 19:16:49 +0200

    Write some basic phantomcss tests

--- patterns/spec/visual/test.js
+++ patterns/spec/visual/test.js
@@ -1,4 +1,9 @@
-casper.start('http://localhost:3000/')
+casper.start('spec/testproject/_build/index.html')
 .then(function() {
   phantomcss.screenshot('body', 'body');
+})
+.then(function() {
+  casper.click('#rightbar-toggle');
+
+  phantomcss.screenshot('body', 'body-rightbar-active');
 });
