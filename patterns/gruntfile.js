de230d560b1679ee1987e8bf696838f0e1d9ab22
Theodor Vararu <theo@tvararu.ro>
2014-01-28 16:43:52 +0200

    Red test wasn't really reliable, changed the way the shell commands get executed

--- patterns/gruntfile.js
+++ patterns/gruntfile.js
@@ -1,54 +1,45 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     phantomcss: {
-      // largeUX: {
-      //   options: {
-      //     screenshots: 'test/screenshots/ux/large/',
-      //     results: 'test/results/ux/large',
-      //     viewportSize: [1200, 800]
-      //   },
-      //   src: ['test/ux.js']
-      // },
-      // mediumUX: {
-      //   options: {
-      //     screenshots: 'test/screenshots/ux/medium/',
-      //     results: 'test/results/ux/medium',
-      //     viewportSize: [991, 800]
-      //   },
-      //   src: ['test/ux.js']
-      // },
-      // smallUX: {
-      //   options: {
-      //     screenshots: 'test/screenshots/ux/small/',
-      //     results: 'test/results/ux/small',
-      //     viewportSize: [767, 480]
-      //   },
-      //   src: ['test/ux.js']
-      // },
-      // xsmallUX: {
-      //   options: {
-      //     screenshots: 'test/screenshots/ux/xsmall/',
-      //     results: 'test/results/ux/xsmall',
-      //     viewportSize: [320, 480]
-      //   },
-      //   src: ['test/ux.js']
-      // },
+      largeUX: {
+        options: {
+          screenshots: 'test/screenshots/ux/large/',
+          results: 'test/results/ux/large',
+          viewportSize: [1200, 800]
+        },
+        src: ['test/ux.js']
+      },
+      mediumUX: {
+        options: {
+          screenshots: 'test/screenshots/ux/medium/',
+          results: 'test/results/ux/medium',
+          viewportSize: [991, 800]
+        },
+        src: ['test/ux.js']
+      },
+      smallUX: {
+        options: {
+          screenshots: 'test/screenshots/ux/small/',
+          results: 'test/results/ux/small',
+          viewportSize: [767, 480]
+        },
+        src: ['test/ux.js']
+      },
+      xsmallUX: {
+        options: {
+          screenshots: 'test/screenshots/ux/xsmall/',
+          results: 'test/results/ux/xsmall',
+          viewportSize: [320, 480]
+        },
+        src: ['test/ux.js']
+      },
       overrides: {
         options: {
           screenshots: 'test/screenshots/overrides/',
-          results: 'test/results/overrides',
+          results: 'test/results/overrides/',
           viewportSize: [320, 480]
         },
         src: ['test/overrides.js']
-      }
-    },
-    shell: {
-      lfacompile: {
-        options: {
-          stdout: true
-        },
-        command: 'cd test/projects/ux/ && ../../../../bin/lfa compile --no-compress && ' +
-                 'cd ../overrides/ && ../../../../bin/lfa compile --no-compress'
       }
     },
     watch: {
@@ -68,9 +59,7 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-phantomcss');
-  grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['phantomcss']);
-  grunt.registerTask('compile', ['shell']);
 };
--- patterns/test/overrides.js
+++ patterns/test/overrides.js
@@ -1,6 +1,6 @@
 casper.start('test/projects/overrides/_build/index.html')
 .then(function() {
-  // overrides: Screenshot the body.
-  phantomcss.screenshot('body', 'overrides');
+  // overrides: Screenshot the button, it should be red.
+  phantomcss.screenshot('.btn.btn-success', 'btn_success_override');
 })
 ;
