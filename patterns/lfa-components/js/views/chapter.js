3e09e454673c0c4c0473446f86c77735bfafdb22
Theodor Vararu <theo@tvararu.ro>
2014-04-17 17:16:09 +0300

    Add next/previous buttons to +article.

--- patterns/lfa-components/js/views/chapter.js
+++ patterns/lfa-components/js/views/chapter.js
@@ -19,6 +19,38 @@ define([
       
       window.App.book.trigger('render', { chapter: chapter });
       
+      // Determine the next and previous chapters.
+      var next = null, previous = null;
+      var $chapters = $('#leftbar li');
+      
+      if ($chapters.length > 1) {
+        for (var i = 0, len = $chapters.length; i < len; i++) {
+          if ($($chapters[i]).hasClass('active')) {
+            if (i === 0) {
+              previous = $chapters[len - 1];
+              next = $chapters[i + 1];
+            } else if (i === (len - 1)) {
+              previous = $chapters[i - 1];
+              next = $chapters[0];
+            } else {
+              previous = $chapters[i - 1];
+              next = $chapters[i + 1];
+            }
+          }
+        }
+      } else {
+        next = previous = $($chapters[0]);
+      }
+      var nextTitle = $(next).children('a').children('span.title').html(),
+          previousTitle = $(previous).children('a').children('span.title').html();
+      
+      next = $(next).children('a').prop('href');
+      previous = $(previous).children('a').prop('href');
+      
+      // Set them in HTML.
+      $('#btn-previous').prop('href', previous).html(nextTitle);
+      $('#btn-next').prop('href', next).html(previousTitle);
+      
       // After the content loads, juice it up with some javascript.
       
       // Enable responsive tables.
