def1343a44e8c6bfbb4af3a4574ec63e03fa0056
Marius Petcu <dapetcu21@gmail.com>
2014-07-10 15:10:50 +0300

    Added android, msie to Modernizr

--- patterns/lfa-components/js/main.js
+++ patterns/lfa-components/js/main.js
@@ -98,8 +98,41 @@ require([
     return navigator.userAgent.match(/iPod/i);
   });
 
+  Modernizr.addTest('android', function () {
+    return navigator.userAgent.match(/Android/i);
+  });
+
   Modernizr.addTest('appleios', function () {
     return (Modernizr.ipad || Modernizr.ipod || Modernizr.iphone);
+  });
+
+  var ua = navigator.userAgent, r;
+  var verIE = null;
+  var isIE = (r = ua.match(/MSIE\s+([0-9]+)/)) || (ua.match(/Trident/) && (r = ua.match(/rv:([0-9]+)/)));
+  if (isIE) {
+    if (r && r.length >= 2 && !isNaN(parseInt(r[1]))) {
+      verIE = parseInt(r[1]);
+    }
+  }
+
+  Modernizr.addTest('msie', function () {
+    return isIE;
+  });
+
+  Modernizr.addTest('msie10', function () {
+    return isIE && verIE === 10;
+  });
+
+  Modernizr.addTest('msie10p', function () {
+    return isIE && verIE >= 10;
+  });
+
+  Modernizr.addTest('msie11', function () {
+    return isIE && verIE === 11;
+  });
+
+  Modernizr.addTest('msie11p', function () {
+    return isIE && verIE >= 11;
   });
 
   // Execute textbook-specific javascript, if it exists.
