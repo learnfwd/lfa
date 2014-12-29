298b0d0af66ea0ba0b2afae81c13f2eb2c193a0a
Marius Petcu <dapetcu21@gmail.com>
2014-12-29 18:17:01 +0200

    First rounds of tests

--- test/basic.js
+++ test/basic.js
@@ -1,4 +1,4 @@
-/*global describe, it */
+/*global describe, it*/
 'use strict';
 var assert = require('assert');
 var LFA = require('../');
@@ -6,5 +6,11 @@ var LFA = require('../');
 describe('lfa node module', function () {
     it('must be a function', function () {
         assert(typeof(LFA) === 'function', 'LFA does not export a function');
+    });
+
+    it('must not be created directly', function () {
+      (function () {
+        new LFA();
+      }).should.throw();
     });
 });
---
+++ test/path-parser.js
@@ -0,0 +1,49 @@
+/*global describe, it */
+'use strict';
+var LFA = require('../');
+var path = require('path');
+
+var fixtures = path.join(__dirname, 'fixtures');
+var basicFixture = path.join(fixtures, 'basic');
+var noKeywordFixture = path.join(fixtures, 'no-keyword');
+
+describe('path parser', function () {
+    it('should load paths correctly', function () {
+      return LFA.loadPaths(basicFixture).then(function(config) {
+        config.projectPath.should.equal(basicFixture);
+        config.packagePath.should.equal(path.join(basicFixture, '.lfa', 'package.json'));
+        config.buildPath.should.equal(path.join(basicFixture, '.lfa', 'build', 'debug'));
+      });
+    });
+
+    it('should load paths correctly from subdirectory', function () {
+      return LFA.loadPaths(path.join(basicFixture, 'text')).then(function(config) {
+        config.projectPath.should.equal(basicFixture);
+        config.packagePath.should.equal(path.join(basicFixture, '.lfa', 'package.json'));
+        config.buildPath.should.equal(path.join(basicFixture, '.lfa', 'build', 'debug'));
+      });
+    });
+
+    it('should accept config at path load time', function () {
+      var conf = { path: basicFixture, randomConf: 'something' };
+      return LFA.loadPaths(conf).then(function(config) {
+        config.randomConf.should.equal(conf.randomConf);
+      });
+    });
+
+    it('should error on wrong path', function () {
+      return LFA.loadPaths(basicFixture + '_some_random_gibberish_').then(function () {
+        throw new Error('Should have errored out on inexistent path');
+      }).catch(function(err) {
+        err.code.should.equal('ENOENT');
+      });
+    });
+
+    it('should error on wrong type of package', function () {
+      return LFA.loadPaths(noKeywordFixture).then(function () {
+        throw new Error('Should have errored out on non-lfa project');
+      }).catch(function(err) {
+        err.message.should.equal('This is not the package.json of a LFA book');
+      });
+    });
+});
