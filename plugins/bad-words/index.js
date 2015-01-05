0fc148e247f93ab4e843dc1152861d2e9a0eac1b
Marius Petcu <dapetcu21@gmail.com>
2015-01-05 17:41:32 +0200

    Deleted test plugins

--- plugins/bad-words/index.js
+++
@@ -1,7 +0,0 @@
-var replace = require('gulp-replace');
-
-module.exports = function (lfa) {
-  lfa.task('html:filter:bad-words', function (input) {
-    return input.pipe(replace(/Foo/, 'Bar'));
-  });
-};
--- plugins/pure-html/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (lfa) {
-  lfa.task('html:compiler:html', function () {
-    return lfa.src(lfa.projectPath + '/**/*.html');
-  });
-};
