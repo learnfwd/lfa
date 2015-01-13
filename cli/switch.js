9fad9d331a72671c7dd64221f93b5ebcd9eef184
Marius Petcu <dapetcu21@gmail.com>
2015-01-13 16:37:57 +0200

    Proper handling of mismatched lfa versions

--- cli/switch.js
+++ cli/switch.js
@@ -3,35 +3,101 @@ var childProcess = require('child_process');
 var when = require('when');
 var path = require('path');
 var chalk = require('chalk');
+var inquirer = require('inquirer');
+var fs = require('fs');
+var nodefn = require('when/node');
 
-var noLFAError = [
+var noEngineField = [
   chalk.yellow('WARNING: '),
-  'LFA is not a local dependency of the project. Please run ',
-  chalk.yellow('lfa upgrade-project')
+  'Your package.json doesn\'t specify the compatible lfa version in the ',
+  chalk.blue('engines'),
+  ' field'
+].join('');
+
+var outdatedLFA = [
+  chalk.red('ERROR: '),
+  'This project requires a newer version of lfa. Please update lfa by running ',
+  chalk.blue('npm update -g lfa')
 ].join('');
 
 var notSatisfied = [
   chalk.yellow('WARNING: '),
-  'The globally-installed LFA doesn\'t satisfy the project\'s dependency field. We encourage you to run ',
+  'This project requires a different (possibly older) version of lfa. We encourage you to run ',
   chalk.yellow('lfa upgrade-project'),
-  '. Will now switch to the project\'s copy.'
+  ' to upgrade your project.'
 ].join('');
+
+var installLFAInfo = 'However, for the time being, you can download and install the older version of lfa inside the project folder and continue working.';
+var installLFAPrompt = 'Would you want to do that now?';
+
+var cantContinue = [
+  chalk.red('ERROR: '),
+  'Can\'t continue without a compatible version of lfa. Will quit.'
+].join('');
+
+var foundLFA = [
+  chalk.blue('INFO: '),
+  'Found locally-installed lfa. Switching to that'
+].join('');
+
+function installLFA(projModulePath, requiredVersion) {
+  console.log('Installing lfa@' + requiredVersion + ' ' + projModulePath);
+}
 
 module.exports = function switchControl(cli, config) {
   if (cli.flags.switch === false) {
     return config;
   }
 
-  var deps = config.package.dependencies;
-  var required = (typeof(deps) === 'object') ? deps.lfa : undefined;
-  var version = cli.pkg.version;
+  var myVersion = cli.pkg.version;
+  var engines = config.package.engines || {};
+  var requiredVersion = (typeof(engines) === 'object') ? engines.lfa : null;
+  if (!requiredVersion || typeof(requiredVersion) !== 'string') {
+    console.log(noEngineField);
+    return config;
+  }
 
-  if (!required) {
-    console.log(noLFAError);
-  } else if (!semver.satisfies(version, required)) {
-    console.log(notSatisfied);
-    var lfaPath = path.join(path.dirname(config.packagePath), 'node_modules', 'lfa', 'cli');
+  if (semver.satisfies(myVersion, requiredVersion)) {
+    return config;
+  }
 
+  if (semver.ltr(myVersion, requiredVersion)) {
+    console.log(outdatedLFA);
+    process.exit(1);
+  }
+
+  console.log(notSatisfied);
+
+  var projModulePath = path.dirname(config.packagePath);
+  var lfaPath = path.join(projModulePath, 'node_modules', 'lfa', 'cli');
+  return nodefn.call(fs.stat, lfaPath).then(function () {
+    return true;
+  }, function () {
+    return false;
+  }).then(function (exists) {
+    // if lfa isn't locally installed, ask to install it
+    if (!exists) {
+      console.log(installLFAInfo);
+      return when.promise(function (resolve) {
+        inquirer.prompt({
+          message: installLFAPrompt,
+          name: 'install',
+          type: 'confirm',
+          default: false,
+        }, function (ans) {
+          resolve(ans.install);
+        });
+      }).then(function (install) {
+        if (!install) {
+          console.log(cantContinue);
+          process.exit(1);
+        }
+      }).then(function () {
+        return installLFA(projModulePath, requiredVersion);
+      });
+    }
+    console.log(foundLFA);
+  }).then(function () {
     var args = process.argv.slice(2);
     args.push('--no-switch');
 
@@ -46,10 +112,8 @@ module.exports = function switchControl(cli, config) {
       }
       process.exit(code);
     });
-    
+      
     return when.promise(function () {}); //Endless promise to stall execution
-  }
-
-  return config;
+  });
 };
 
--- src/config-parser.js
+++ src/config-parser.js
@@ -1,6 +1,15 @@
 var _ = require('lodash');
 var assert = require('assert');
 
+function idToTitle(bookId) {
+  //Get rid of commas and capitalize each word
+  return bookId.replace(/-/, ' ').replace(/[^\s]+/g, function(word) {
+    return word.replace(/^./, function(first) {
+      return first.toUpperCase();
+    });
+  });
+}
+
 module.exports = function parseConfig(config) {
   assert(typeof(config.packagePath) === 'string', 'config.packagePath must be a string');
   assert(typeof(config.projectPath) === 'string', 'config.projectPath must be a string');
@@ -10,23 +19,14 @@ module.exports = function parseConfig(config) {
 
   var packageJson = config.package;
 
-  function idToTitle(bookId) {
-    //Get rid of commas and capitalize each word
-    return bookId.replace(/-/, ' ').replace(/[^\s]+/g, function(word) {
-      return word.replace(/^./, function(first) {
-        return first.toUpperCase();
-      });
-    });
-  }
-
   config.bookId = packageJson.name;
   assert(typeof(config.bookId) === 'string', 'packageJson.name must be a string');
   assert(/^[a-z0-9-]+$/.test(config.name), 'packageJson.name must only contain lowercase latin letters, digits and dashes');
 
-  config.meta = packageJson.meta || {};
-  assert(typeof(config.meta) === 'object', 'packageJson.meta must be an object');
+  config.book = packageJson.book || {};
+  assert(typeof(config.book) === 'object', 'packageJson.book must be an object');
 
-  config.meta.title = config.meta.title || idToTitle(config.bookId);
+  config.book.title = config.book.title || idToTitle(config.bookId);
 
   config.defaultTask = packageJson.defaultTask || 'default';
 
