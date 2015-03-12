a74f6673f4a80e111331618f7eb7865c5e257fde
Marius Petcu <dapetcu21@gmail.com>
2015-03-12 19:15:05 +0200

    Rudimentary global require mechanism

--- plugins/lfa-core/frontend/js/index.js
+++ plugins/lfa-core/frontend/js/index.js
@@ -1,3 +1,15 @@
+// Export require globally with limited access
+var resolveMap = {
+  'jquery': require.resolve('jquery'),
+  'lodash': require.resolve('lodash'),
+  'lfa-core/app': require.resolve('lfa-core/app'),
+  'app': require.resolve('lfa-core/app'),
+};
+
+window.require = function (mod) {
+  return __webpack_require__(resolveMap[mod]);
+};
+
 // Bootstrap our app
 var MainView = require('./views/main-view');
 new MainView();
