25f210c6e0ea63086ba937a7588d25f9b808f5d2
Marius Petcu <dapetcu21@gmail.com>
2015-04-16 17:44:49 +0300

    Moved lfa-core dependencies to globally-accessible place

--- plugins/lfa-core/web_modules/backbone.js
+++ plugins/lfa-core/web_modules/backbone.js
@@ -1,4 +1,4 @@
 // Backbone from npm comes without jQuery
-var Backbone = require('../../../../../node_modules/backbone');
+var Backbone = require('../../../node_modules/backbone');
 Backbone.$ = require('jquery');
 module.exports = Backbone;
