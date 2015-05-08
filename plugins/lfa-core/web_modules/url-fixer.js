847a903945036ffda226b32552befa4acfe2469b
Marius Petcu <dapetcu21@gmail.com>
2015-05-08 11:57:44 +0300

    CSS url() is more reliable now

--- plugins/lfa-core/web_modules/url-fixer.js
+++ plugins/lfa-core/web_modules/url-fixer.js
@@ -1,4 +1,11 @@
 module.exports = function (content) {
   this.cacheable(true);
-  return content.replace(/url\((\\?['"])?\.?\//g, 'url($1", window.location.origin, "/');
+  return content.replace(/url\(((\\?['"])?)([^\)]+)\1\)/g, function (match, sep, p2, url) {
+      if (/^\/\//.test(url)) {
+        url = '", window.location.protocol, "' + url;
+      } else if (!/:\/\//.test(url)) {
+        url = '", window.location.origin, "/' + url;
+      }
+      return 'url(' + sep + url + sep + ')';
+  });
 };
