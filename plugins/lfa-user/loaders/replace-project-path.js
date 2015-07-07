96392726ccd7ce47195d6295ec785c21788321c3
Marius Petcu <dapetcu21@gmail.com>
2015-07-07 16:18:42 +0300

    Fixed path issues on Windows

--- plugins/lfa-user/loaders/replace-project-path.js
+++ plugins/lfa-user/loaders/replace-project-path.js
@@ -1,7 +1,11 @@
 var path = require('path');
+var sep = path.sep.replace('\\', '\\\\');
 
 module.exports = function (content) {
   this.cacheable(true);
-  var lfa = this.options.lfa;
-  return content.replace(/__PROJ_PATH__/, lfa.config.projectPath);
+  var projPath = this.options.lfa.config.projectPath.split(path.sep).join(sep);
+
+  return content.replace(/('[^']*|"[^"]*)__PROJ_PATH__([^'"!]*)([^'"]*('|"))/, function (match, p1, p2, p3) {
+    return p1 + projPath + p2.split('/').join(sep) + p3;
+  });
 };
