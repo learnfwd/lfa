a04421ddfc0637d95cfe205a93d2cf994053008b
Marius Petcu <dapetcu21@gmail.com>
2017-03-03 00:20:02 +0200

    Silence optional require warnings

--- src/webpack-process-stats.js
+++ src/webpack-process-stats.js
@@ -2,7 +2,7 @@ var _ = require('lodash');
 
 module.exports = function (stats) {
   stats.warnings = _.filter(stats.warnings || [], function (warning) {
-    return (warning.indexOf('Module not found: Error: Cannot resolve \'file\' or \'directory\'') === -1);
+    return (warning.indexOf('Module not found: Error: Can\'t resolve ') === -1);
   });
   return stats;
 };
