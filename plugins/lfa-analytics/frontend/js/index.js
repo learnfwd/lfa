b6c23992f27b1dac905c2ced5347289f4ebde495
Paul Balogh <paul.balogh@gmail.com>
2019-07-08 17:06:38 +0100

    Remove intercom

--- plugins/lfa-analytics/frontend/js/index.js
+++ plugins/lfa-analytics/frontend/js/index.js
@@ -6,7 +6,7 @@ var _ = require('lodash');
 
 // var MixpanelAnalytics = require('./mixpanel');
 var GoogleAnalytics = require('./ga');
-var IntercomAnalytics = require('./intercom');
+// var IntercomAnalytics = require('./intercom');
 
 function AnalyticsManager() {
   this.sessionId = _getSessionId();
@@ -21,7 +21,7 @@ function AnalyticsManager() {
 
   // this._registerAnalyticsBackend(new MixpanelAnalytics(this));
   this._registerAnalyticsBackend(new GoogleAnalytics(this));
-  this._registerAnalyticsBackend(new IntercomAnalytics(this));
+  // this._registerAnalyticsBackend(new IntercomAnalytics(this));
 }
 
 AnalyticsManager.prototype._registerAnalyticsBackend = function(be) {
@@ -30,7 +30,8 @@ AnalyticsManager.prototype._registerAnalyticsBackend = function(be) {
 
 function _makeUUID() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r&0x3 | 0x8);
+    var r = (Math.random() * 16) | 0,
+      v = c === 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 }
@@ -45,20 +46,20 @@ function _getSessionId() {
   return sessionId;
 }
 
-AnalyticsManager.prototype.trackEvent = function (key, data) {
+AnalyticsManager.prototype.trackEvent = function(key, data) {
   var self = this;
-  _.each(self._backends, function (backend) {
+  _.each(self._backends, function(backend) {
     backend.trackEvent(self, key, data);
   });
 };
 
 var analyticsManager = new AnalyticsManager();
 
-App.on('static', function (key, data) {
+App.on('static', function(key, data) {
   analyticsManager.trackEvent(key, data);
 });
 
-App.book.on('render', function (opts) {
+App.book.on('render', function(opts) {
   analyticsManager.trackEvent('navigate', { route: opts.chapter });
 });
 
